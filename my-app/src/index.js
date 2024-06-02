972742bd0cf7bcfe8294c54620a392d084c88d60
Mario <mario.prskalo5@gmail.com>
2024-06-02 18:39:27 +0200

    change

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,7 +8,6 @@ import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
