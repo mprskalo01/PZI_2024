73cdbaca201d3efc755b458b3c27683be8deaa3e
Mario <mario.prskalo5@gmail.com>
2024-09-19 11:09:30 +0200

    change api

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://fpmozcars.studenti.sum.ba/",
-  baseURL: "http://localhost:5002/",
+  // baseURL: "http://localhost:5002/",
+  baseURL: "https://fpmozcars.onrender.com",
 });
 
 export default API;
